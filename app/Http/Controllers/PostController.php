7331e7a8dda3558486551088032b84d65e758dc7
Augusto Soto <sotoaugustoa@gmail.com>
2023-10-15 14:46:56 -0600

    create dashboard and open it when finish register

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['show', 'index']);
+    }
+
+    public function index(User $user)
+    {
+        // $posts = Post::where('user_id', $user->id)->latest()->paginate(20);
+
+        return view('dashboard', [
+            'user' => $user,
+            // 'posts' => $posts
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 
 /*
@@ -25,7 +27,7 @@ Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 
-
+Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
